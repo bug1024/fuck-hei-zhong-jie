04d57d9dbca3bf6c4ea2126db61fd1a9f13af0b1
bug1024 <bug1024.com@gmail.com>
2017-06-27 23:10:18 +0800

    自定义信息窗口

--- resources/views/map.blade.php
+++ resources/views/map.blade.php
@@ -14,6 +14,8 @@
     <div id="allmap" data-map="{{ json_encode($agents)}}"></div>
 </body>
 
+<script src="https://cdn.bootcss.com/jquery/3.2.1/jquery.min.js"></script>
+
 <script type="text/javascript">
     // 百度地图API功能
     var map = new BMap.Map("allmap");
@@ -26,10 +28,30 @@
     // 开启鼠标滚轮缩放
     map.enableScrollWheelZoom(true);
     // 编写自定义函数,创建标注
-    function addMarker(point){
-      var marker = new BMap.Marker(point);
-      map.addOverlay(marker);
+    function addMarker(point, title){
+        var marker = new BMap.Marker(point);
+        map.addOverlay(marker);
+        var opts = {
+            width : 200,
+            height: 100,
+            title : title
+        }
+        var infoWindow = new BMap.InfoWindow("fuck中介", opts);
+        marker.addEventListener("click", function(){
+            map.openInfoWindow(infoWindow, point);
+        });
     }
+
+    var $map = $("#allmap");
+    var data = $map.data("map");
+    $.each(data, function(key, value) {
+        var arr = value.pos.split(",");
+        var lng = arr[0];
+        var lat = arr[1];
+        var point = new BMap.Point(lng, lat);
+        addMarker(point, value.name + " " + value.mobile);
+        console.log(value);
+    });
 </script>
 
 </html>
